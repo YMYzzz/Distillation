ab5bdba8a966c1a3d8738395d4dd78a10e0415f5
Asteriska <1758995741@qq.com>
2022-06-22 11:24:33 +0800

    对接后端验证接口

--- src/component/Register/index.jsx
+++ src/component/Register/index.jsx
@@ -150,11 +150,11 @@ const RegisterMod = () => {
                         clearInterval(t1)
                     }
                 },1000);
-                const data = { 'phone': phoneNum };
+               // const data = {  };
                 const requestOptions = {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({ data })
+                    body: JSON.stringify({ 'phone': phoneNum })
                 };
                 const response = await fetch('http://127.0.0.1:5000/api/user/valid_code', requestOptions);
                 const result = await response.json();
